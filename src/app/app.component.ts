ec563c699adb40768bb8746928da9122b5b5d6ff
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-01-25 08:28:12 +0100

    changed the project into --no-standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title: string = "basket of baby beavers";
+  imgSrc: string ="https://cwhl.vet.cornell.edu/system/files/media/baby%20beavers%20%286%29.JPG"
+  constructor() {   
+  }
 }
