106874ec8a228ae9c5c9014a79cc442cb17ad66e
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-02-05 09:35:05 +0100

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-interface Pokemon {
-  id: number,
-  name: string,
-  type: string,
-  isCool: boolean
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  pokemons: Pokemon[] = [{
-    id: 1,
-    name: 'pikachu',
-    type: 'electric',
-    isCool: false
-  }, {
-    id: 2,
-    name: 'squirtle',
-    type: 'water',
-    isCool: true
-  }, {
-    id: 3,
-    name: 'charmander',
-    type: 'fire',
-    isCool: true
-  }]
+  pokemonName: string = "";
 
   constructor() {
+  }
+
+  handleChange(event : any){
+    this.pokemonName = event?.target.value;
   }
 }
