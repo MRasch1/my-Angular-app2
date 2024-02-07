772d9432e0ceed8236c70a2186015914d81c1f4c
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-02-07 12:58:00 +0100

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,43 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Pokemon {
+  id: number,
+  name: string,
+  type: string,
+  isCool: boolean,
+  isStylish: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  pokemonName: string = "";
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'pikachu',
+      type: 'electric',
+      isCool: false,
+      isStylish: true
+    },
+    {
+      id: 2,
+      name: 'squirtle',
+      type: 'water',
+      isCool: true,
+      isStylish: true
+    },
+    {
+      id: 3,
+      name: 'charmander',
+      type: 'fire',
+      isCool: true,
+      isStylish: false
+    },
+  ];
 
-  constructor() {
-  }
-
-  handleChange(event : any){
-    this.pokemonName = event?.target.value;
-  }
+  constructor() {}
 }
