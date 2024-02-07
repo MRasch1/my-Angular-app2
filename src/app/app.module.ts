f9a716f02ae88edc286652f684b179ee91dcc93d
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-02-07 14:37:34 +0100

    Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    PokemonBaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
