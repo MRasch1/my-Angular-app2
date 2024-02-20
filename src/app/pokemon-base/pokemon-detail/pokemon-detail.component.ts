aa834102f8413e3141485a5e0662836b851ad018
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-02-20 14:25:19 +0100

    List/Detail Page (@Input Explained)

---
+++ src/app/pokemon-base/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from '../../models/pokemon';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  templateUrl: './pokemon-detail.component.html',
+  styleUrl: './pokemon-detail.component.css'
+})
+export class PokemonDetailComponent implements OnInit {
+  @Input()
+  detail!: Pokemon;
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../../models/pokemon';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrl: './pokemon-list.component.css'
+  styleUrl: './pokemon-list.component.css',
 })
 export class PokemonListComponent implements OnInit {
-  pokemon: string;
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'pikachu',
+      type: 'electric',
+      isCool: false,
+      isStylish: true,
+    },
+    {
+      id: 2,
+      name: 'squirtle',
+      type: 'water',
+      isCool: true,
+      isStylish: true,
+    },
+    {
+      id: 3,
+      name: 'charmander',
+      type: 'fire',
+      isCool: true,
+      isStylish: false,
+    },
+  ];
 
   constructor() {
-    this.pokemon ="";
+
   }
 
   ngOnInit(): void {
-    console.log("ngOninitFired");
-    this.pokemon = "";
+
   }
-
-
 }
