724dfaa438244c558006d955029510c40a3667a8
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-02-26 08:58:44 +0100

    Observables

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,37 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Pokemon } from '../models/pokemon';
+import { Observable } from 'rxjs';
+
+const POKEMON_API = 'http://localhost:3000/pokemon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-constructor() { }
+constructor(private http: HttpClient) { }
 
-getPokemons() : Pokemon[] {
-  return [
-    {
-      id: 1,
-      name: 'pikachu',
-      type: 'electric',
-      isCool: false,
-      isStylish: true,
-    },
-    {
-      id: 2,
-      name: 'squirtle',
-      type: 'water',
-      isCool: true,
-      isStylish: true,
-    },
-    {
-      id: 3,
-      name: 'charmander',
-      type: 'fire',
-      isCool: true,
-      isStylish: false,
-    },
-  ];
+getPokemons() : Observable<Pokemon[]> {
+  return this.http.get<Pokemon[]>(POKEMON_API);
 }
 
 }
