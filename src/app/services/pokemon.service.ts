ff14583d0115732d646435caae3909718a48efbf
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-02-26 09:04:38 +0100

    deleted extra line

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Pokemon } from '../models/pokemon';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
