fcc4b0ac73e5d711b9ef1a0f1b1325b648d022f8
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-03-12 10:32:16 +0100

    @ViewChild()

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { Pokemon } from '../../models/pokemon';
 import { PokemonService } from '../../services/pokemon.service';
 
@@ -7,11 +7,15 @@ import { PokemonService } from '../../services/pokemon.service';
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.css',
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, AfterViewInit {
 pokemons!: Pokemon[];
+@ViewChildren('pokemonRef') pokemonRef!: ElementRef;
 
   constructor(private pokemonService: PokemonService) {
 
+  }
+  ngAfterViewInit(): void {
+    console.log(this.pokemonRef);
   }
 
   handleRemove(event: Pokemon){
