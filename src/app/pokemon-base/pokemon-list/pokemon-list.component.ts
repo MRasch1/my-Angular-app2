b4af8901c5e389f73f077aa2f51edbd5de674df8
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2024-03-12 13:54:25 +0100

    ElementRef

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ContentChildren, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { Pokemon } from '../../models/pokemon';
 import { PokemonService } from '../../services/pokemon.service';
+import { PokemonDetailComponent } from '../pokemon-detail/pokemon-detail.component';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -8,24 +9,32 @@ import { PokemonService } from '../../services/pokemon.service';
   styleUrl: './pokemon-list.component.css',
 })
 export class PokemonListComponent implements OnInit, AfterViewInit {
-pokemons!: Pokemon[];
-@ViewChildren('pokemonRef') pokemonRef!: ElementRef;
+  pokemons!: Pokemon[];
+  @ViewChildren('pokemonRef') pokemonRef!: ElementRef;
+  @ViewChild('pokemonTh') pokemonTh!: ElementRef;
+  @ContentChildren(PokemonDetailComponent) contentList!: any;
 
   constructor(private pokemonService: PokemonService) {
 
   }
-  ngAfterViewInit(): void {
-    console.log(this.pokemonRef);
+
+  ngAfterContentinit(): void{
+
   }
 
-  handleRemove(event: Pokemon){
+  ngAfterViewInit(): void {
+    console.log(this.pokemonTh);
+    this.pokemonTh.nativeElement.innerText = "Pokemon Name";
+  }
+
+  handleRemove(event: Pokemon) {
     this.pokemons = this.pokemons.filter((pokemon: Pokemon) => {
       return pokemon.id !== event.id;
-    })
+    });
   }
 
   ngOnInit(): void {
-    this.pokemonService.getPokemons().subscribe((data:Pokemon[]) => {
+    this.pokemonService.getPokemons().subscribe((data: Pokemon[]) => {
       console.log(data);
       this.pokemons = data;
     });
